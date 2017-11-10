6f85b189711daa5a8c1733bc83ce0699e57a9691
Chris Brombach <chribrom@gmail.com>
2017-11-10 17:36:36 +0100

    bug

--- MPU6050/mpu6050.h
+++ MPU6050/mpu6050.h
@@ -14,10 +14,13 @@ public:
 private:
 
 
+	int fd_1; 
+	int fd_2; 
+
 	int accDataBuffer[12]; 
 	int gyroDataBuffer[12]; 
 	int accMergeBuffer[6];
 	int gyroMergeBuffer[6];  
-}
+};
 
 #endif /* RASP_MPU6050_H */
