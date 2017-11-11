6a1c25abd68f6390ff0ead07803c0b2a69847652
Chris Brombach <chribrom@gmail.com>
2017-11-11 15:47:24 +0100

    Bug fix

--- PCA9685/PCA9685.cpp
+++ PCA9685/PCA9685.cpp
@@ -11,7 +11,7 @@
 #define MODE_1 0x00
 
 #define LED0_ON_L 0x06
-#define LEDO_ON_H 0x07
+#define LED0_ON_H 0x07
 #define LED0_OFF_L 0x08
 #define LED0_OFF_H 0x09
 
@@ -22,6 +22,9 @@
 
 #define ON 0x00
 #define OFF 0x10 
+
+#define LED_FULL_ON 0x10
+#define LED_FULL_OFF 0x10
 
 
 #define PRE_SCALE_VALUE 0x79 // round(osc_clock/(4096*update_rate))-1, = round(25 MHz/(4096*50Hz))-1 = 121 = 0x79
@@ -43,14 +46,14 @@ void PCA9685::setDutyCycle(int channel, float percentage){
 	if(percentage == 100.0){
 		wiringPiI2CWriteReg8(fd,LED0_OFF_H, 0x00);
 		wiringPiI2CWriteReg8(fd,LED0_OFF_H, 0x00);
-		wiringPiI2CWriteReg8(fd,LED0_ON_H, ON);
+		wiringPiI2CWriteReg8(fd,LED0_ON_H, LED_FULL_ON);
 	}else if(percentage == 0){
 		wiringPiI2CWriteReg8(fd,LED0_ON_H, 0x00);
 		wiringPiI2CWriteReg8(fd,LED0_ON_H, 0x00);
-		wiringPiI2CWriteReg8(fd,LED0_OFF_H, OFF);
+		wiringPiI2CWriteReg8(fd,LED0_OFF_H, LED_FULL_OFF);
 	}else{
 		uint16_t value = 40.95 * percentage; 
-		wiringPiI2CWriteReg8(fd,LEDO_ON_H, 0x00);
+		wiringPiI2CWriteReg8(fd,LED0_ON_H, 0x00);
 		wiringPiI2CWriteReg8(fd,LED0_ON_L, 0x00);
 
 		wiringPiI2CWriteReg8(fd,LED0_OFF_L, value); 
